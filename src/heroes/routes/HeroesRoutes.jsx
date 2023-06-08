19d5c3c0bd65b04cf080a83391ad91d64f667c7b
Daniel Gizzi <mut_dgizzi@externos.entel.cl>
2023-06-07 23:51:40 -0400

    update div container

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -8,7 +8,7 @@ export const HeroesRoutes = () => {
     return (
         <>
             <Navbar />
-            <div className="mt-5 px-10 ">
+            <div className="mt-5 px-10 sm:mx-auto sm:px-0 sm:max-w-xl  md:max-w-2xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl">
                 <Outlet />
             </div>
         </>
